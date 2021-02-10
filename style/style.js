3be305b4e86c2bd3e4a57aa95528229ef90af536
toledoma <toledoma@g.crookcountyschools.orggit config --global user.name marissatoledo>
2021-02-10 10:04:03 -0800

    added java

---
+++ style/style.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//Hello World for proof of life
+console.log('Hello');
+
+var myVar =5;
+console.log('myVar is: ',mayVar);
+
+console.log('myVar plus 3:', myVar +3);
+ var myString= 'very cool'
+var myAddOn= 'this is the best language in the world'
+
+console.log(myString ++ +myAddOn);
