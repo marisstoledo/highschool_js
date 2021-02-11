c14c69491d2d67db19bd2bb1211e51be048563f0
toledoma <toledoma@g.crookcountyschools.orggit config --global user.name marissatoledo>
2021-02-11 08:18:30 -0800

    added new branch

--- style/app.js
+++ style/app.js
@@ -1,7 +1,17 @@
+function partOne() {
+
 let name = 'marissa';
 let sum = 6 + 4;
 let divide = 10 / 5;
+}
+partOne();
+
+
+function partTwo(dynamicStuff){
+
 
 console.log('name:', name);
 console.log('sum of 6 + 4', sum);
 console.log('10 / 5 is:', divide);
+}
+partTwo('this will be logged')
