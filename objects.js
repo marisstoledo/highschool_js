c9a242fd85eafbfc09e360eb52743847ce87bce5
toledoma <toledoma@g.crookcountyschools.orggit config --global user.name marissatoledo>
2021-03-17 11:44:08 -0700

    did lab 54

---
+++ objects.js
@@ -0,0 +1,18 @@
+let name: 'marissa'
+let lastName: 'toledo'
+let age: '15'
+let school: 'crook county highschool'
+let state:'oregon'
+let favFood:'pasta'
+
+console.log(name)
+console.log(lastName)
+console.log(age)
+console.log(school)
+console.log(state)
+console.log(favFood)
+
+
+
+let favSubjects = [{name:math}, {name:'socail studies'}, {name:'english'}]
+let family = [{dad: 'Roberto'}, {mom:'Alicia'}]
--- script.js
+++ script.js
@@ -3,17 +3,4 @@
 console.log('hello')
 
 
-let name: 'marissa'
-let lastName: 'toledo'
-let age: '15'
-let school: 'crook county highschool'
-let state:'oregon'
-let favFood:'pasta'
-
-console.log(name)
-console.log(lastName)
-console.log(age)
-console.log(school)
-console.log(state)
-console.log(favFood)
 
