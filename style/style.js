74bd98440dba01a2a372a67faec07a118ae1b754
toledoma <toledoma@g.crookcountyschools.orggit config --global user.name marissatoledo>
2021-03-16 21:09:32 -0700

    did lab 52

--- style/style.js
+++ style/style.js
@@ -39,3 +39,18 @@ console.log('number[1] multiply by number[2], multiply by number[5]', number[1]*
 
 
 
+let number = ['2', '6', '4', '10', '3', '7', '5', '8', '9']
+for (i = 0; i < cars.length; i++)
+
+console.log(number[1]* number[1])
+console.log(number[2]* number[2])
+console.log(number[3]* number[3])
+console.log(number[4]* number[4])
+console.log(number[5]* number[5])
+console.log(number[6]* number[6])
+console.log(number[7]* number[7])
+console.log(number[8]* number[8])
+console.log(number[9]* number[9])
+console.log(number[10]* number[10])
+
+
