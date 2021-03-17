4b55fabbf5cd0ae2aae5eef8097181fd863c0d05
toledoma <toledoma@g.crookcountyschools.orggit config --global user.name marissatoledo>
2021-03-17 11:22:19 -0700

    did lab 53

---
+++ script.js
@@ -0,0 +1,19 @@
+'use scrict'
+
+console.log('hello')
+
+
+let name: 'marissa'
+let lastName: 'toledo'
+let age: '15'
+let school: 'crook county highschool'
+let state:'oregon'
+let favFood:'pasta'
+
+console.log(name)
+console.log(lastName)
+console.log(age)
+console.log(school)
+console.log(state)
+console.log(favFood)
+
