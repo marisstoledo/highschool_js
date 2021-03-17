00e308a6c9e9be9ba734954c127b5cbc29aa6002
toledoma <toledoma@g.crookcountyschools.orggit config --global user.name marissatoledo>
2021-03-16 19:48:52 -0700

    added lab 50

--- style/app.js
+++ style/app.js
@@ -22,5 +22,15 @@ function range(start, end) {
     if(start === end) return [start];
     [start, ...range(start + 1, end)].
 }
+
+
   console.log(1, 2, 3, 4, 5, 6 )
+
+let divide= 10 / 2;
+let multiply= 2 * 2 * 2;
+let subtract= 8 - 4; 
+console.log('sum of 2 + 2 + 3 + 4', sum);
+console.log('10 / 5 is:', divide);
+console.log('2 * 2 * 2 is:', multiply);
+console.log('8 - 4 is:', subract)
  
